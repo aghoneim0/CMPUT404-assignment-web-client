0ca0a4a8699c2b189d460f7784810de00700f0d9
aghoneim0 <aghoneim@ualberta.ca>
2014-01-26 14:38:57 -0700

    Http Client
    +Httpclient.py

--- README.md
+++ README.md
@@ -3,17 +3,23 @@ CMPUT404-assignment-web-client
 
 CMPUT404-assignment-web-client
 
-See requirements.org (plain-text) for a description of the project.
+A Basic Http Client made by Ahmed Ghoneim
 
-Make a simple web-client like curl or wget
 
 Contributors / Licensing
 ========================
 
 Generally everything is LICENSE'D under the Apache 2 license by Abram Hindle.
 
-But the server.py example is derived from the python documentation
+httpclient.py contains contributions from:
+
+Abram Hindle 
+Ahmed Ghoneim 
+
+
+But the httpclient.py example is derived from the python documentation
 examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
+
 
--- httpclient.py
+++ httpclient.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-# Copyright 2013 Abram Hindle
+# Copyright 2013 Abram Hindle,Ahmed Ghoneim
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -32,42 +32,152 @@ class HTTPRequest(object):
         self.code = code
         self.body = body
 
+
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    
+    #Creates the Request to be sent 
+    def get_header(self,method,data):
 
-    def connect(self, host, port):
-        # use sockets!
-        return None
+        url  = data[0]
+        host = data[1]
+        port = data[2]
+        content_type = data[3]
+        content_length = data[4]
+        contnet= data[6]
+        line = "\r\n"
+        request = [
+            method+' /'+url+'  HTTP/1.1',
+            'Host: '+host,
+            'Connection: keep-alive',
+            content_type,
+            content_length,
+            "",
+            contnet,
+            "",
+                ]
+        
+        return line.join(request)
 
-    def get_code(self, data):
-        return None
+    # Returns the Host/Port number for the socket to connect to 
+    def get_config(self, url):
 
-    def get_headers(self,data):
-        return None
+        result=[]
+        if '127.0.0.1' in url:
+            temp  = url.split('/')
+            host = temp[2].split(':')[0]
+            port = temp[2].split(':')[1]
+            del temp[0],temp[0],temp[0]
+            temp = '/'.join(temp)
+            result.extend([temp,host,int(port)])
+        else:
+            print url
+            host = url.split('/')[2]
+            url=''
+            port=80
+            result.extend([url,host,port])
 
-    def get_body(self, data):
-        return None
+        return result
 
-    # read everything from the socket
-    def recvall(self, sock):
-        buffer = bytearray()
-        done = False
-        while not done:
-            part = sock.recv(1024)
-            if (part):
-                buffer.extend(part)
-            else:
-                done = not part
-        return str(buffer)
+    def get_code(self,data):
+        data=data.split(' ')
+        data=int(data[1])
+        return data
+
+    def GET1(self, url, args=None):
+        code = 500
+        
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        data= self.get_config(url) # Get Host/Port number to connect to 
+        url = data[0]
+        host= data[1]
+        port= data[2]
+        s.connect((host,port))
+        data.extend(["","","",""])
+
+        request=self.get_header('GET',data)
+        print '====================================GET Request================================='
+      
+        s.send(request)
+        body=''
+        buffer = s.recv(1024)
+        while buffer:
+            body += buffer
+            buffer = s.recv(1024)
+        print '====================================GET Response================================='
+        print body
+        code=self.get_code(body) # Replace Code 500 with response code
+        return HTTPRequest(code, body)
 
     def GET(self, url, args=None):
         code = 500
-        body = ""
-        return HTTPRequest(code, body)
+        
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+        data= self.get_config(url) # Get Host/Port number to connect to 
+        url = data[0]
+        host= data[1]
+        port= data[2]
+        s.connect((host,port))
+        data.extend(["","","",""])
+
+        request=self.get_header('GET',data)
+        print '====================================GET Request================================='
+      
+        s.send(request)
+        body=''
+        
+        
+        buffer = s.recv(1024)
+        body = ''
+        while buffer:
+            body += buffer
+            buffer = s.recv(1024)
+        print '====================================GET Response================================='
+        print body
+        code=self.get_code(body) # Replace Code 500 with response code
+        return HTTPRequest(code, body)        
     def POST(self, url, args=None):
         code = 500
-        body = ""
+        print url
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        data=self.get_config(url) 
+        url = data[0]
+        host= data[1]
+        port= data[2]
+
+        s.connect((host,port)) # Get Host/Port number to connect to 
+       
+        if args == None:
+
+            data.extend(['Content-Type: application/x-www-form-urlencoded',
+            	"",
+            	"",""])
+
+        else:
+        	args=urllib.urlencode(args)
+        	data.extend(['Content-Type: application/x-www-form-urlencoded',
+        	 'Content-Length: %s' % (len(args)),
+        	 	"",
+        	 	args,
+        	 	"",
+        	 	])
+        request=self.get_header('POST',data)
+        print '====================================POST Request================================='
+        s.send(request)
+        buffer = s.recv(1024)
+        response=''
+        while buffer:
+            response += buffer
+            buffer = s.recv(1024)
+        code=response
+        print '====================================POST Response================================='
+        print response
+        code=self.get_code(response) # Replace Code 500 with response code
+        response=response.splitlines()
+        response= response[len(response)-1]  #Json Last Line 
+        body=response
         return HTTPRequest(code, body)
 
     def command(self, url, command="GET", args=None):
@@ -83,6 +193,7 @@ if __name__ == "__main__":
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
-        print client.command( sys.argv[1], sys.argv[2] )
+        print client.command( sys.argv[2], sys.argv[1] )
     else:
-        print client.command( command, sys.argv[1] )    
+        
+        print client.command( sys.argv[1],command)    
